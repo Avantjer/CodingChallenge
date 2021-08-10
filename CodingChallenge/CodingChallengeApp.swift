0305bbcd15e95545910f3974df5189662cc7e9c9
Jerry Barnes <jer4hire@gmail.com>
2021-08-10 10:08:58 -0700

    Initial Commit

---
+++ CodingChallenge/CodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallengeApp.swift
+//  CodingChallenge
+//
+//  Created by Jerry Barnes on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
